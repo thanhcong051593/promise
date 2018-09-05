a047fc4c4efc84ebfa9e4738e344cd61d5c75f06
thanhcong051593 <42972932+thanhcong051593@users.noreply.github.com>
2018-09-05 08:12:06 +0700

    Update promise3.js

--- promise3.js
+++ promise3.js
@@ -1,1 +1,18 @@
+var pro = new Promise(function(resolve, reject){
+	fs.readFile('./data.txt','utf-8', (err, data) => {
+		if(err){
+			console.log("readFile fail");
+			reject(err);
+		}else{
+			console.log("readFile success");
+			resolve(data);
+		}
+	});
+})
 
+ pro.then(function(data){
+ 	console.log(data);
+ })
+ .catch(function(err){
+ 	console.log(err);
+});
